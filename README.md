76500bb03551813dd49833aeaa91b9f2b6305f05
Rob Blackwell <rob@robblackwell.com>
2018-05-10 16:50:45 +0100

    Improved defaults, support for NaN

--- README.md
+++ README.md
@@ -23,13 +23,11 @@ A useful companion to the SimradEK60.jl package.
 using EchogramImages
 using SimradRaw
 using SimradEK60
-using Images
 ps =collect(pings(EK60_SAMPLE));
 ps38 = [p for p in ps if p.frequency == 38000];
 Sv38 = Sv(ps38);
-r = R(ps38);
 img = echogram(Sv38,vmin=-95,vmax=-50)
-imresize(img,(480,640))
+
 ```
 
 You can also use other color schemes
@@ -37,7 +35,6 @@ You can also use other color schemes
 ```
 using ColorSchemes
 img = echogram(Sv38,color=ColorSchemes.plasma)
-imresize(img,(480,640))
 ```
 
 ## Acknowledgements
--- src/EchogramImages.jl
+++ src/EchogramImages.jl
@@ -20,28 +20,54 @@ const EK500_COLOURS = [RGB(1, 1, 1),
                              RGB(166/255, 83/255, 60/255),
                              RGB(120/255, 60/255, 40/255)]
 
-"""
 
+
+"""
+    echogram(A;
+             vmin = nothing,
+             vmax = nothing,
+             color=EK500_COLOURS,
+             size=(480,640))
+
+A is any numeric array
+
+color can be a list of Color or come from ColorSchemes.jl or
+PerceptualColourMaps.
+
+Set size = nothing for full resolution.
+
+vmin and vmax are minimum and maximum values.
 
 """
 function echogram(A;
                   vmin = nothing,
                   vmax = nothing,
                   color=EK500_COLOURS,
-                  size=(640,480))
+                  size=(480,640))
 
 
-    if vmax != nothing && vmin != nothing
-        A = clamp.(A,vmin,vmax)
+    if size != nothing
+        A = imresize(A, size)
     end
 
-    x = convert(AbstractFloat, minimum(A)) # Force floating point arithmetic
-    y = maximum(A)
-    A = (A .- x) ./ (y - x)
+    if vmin == nothing
+        vmin = minimum(A[.!isnan.(A)])
+    end
+
+    if vmax == nothing
+        vmax = maximum(A[.!isnan.(A)])
+    end
+
+    vmin = convert(AbstractFloat, vmin)
+    vmax = convert(AbstractFloat, vmax)
+
     n = length(color)
-    f = s->clamp(round(Int, (n-1)*s)+1, 1, n)  # safely convert 0-1 to 1:n
+
+    g = x -> (clamp(isnan(x)? vmin : x, vmin, vmax) - vmin) / (vmax - vmin)
+    f = s->clamp(round(Int, (n-1)*g(s))+1, 1, n)
+
     Ai = mappedarray(f, A)       # like f.(A) but does not allocate significant memory
-    IndirectArray(Ai, color)      # colormap array
+    IndirectArray(Ai, color)     # colormap array
 
 end
 
