401f6f7fbcf2b48259337ef82b1ddb73c099ece9
Rob Blackwell <rob@robblackwell.com>
2018-05-10 14:22:53 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # EchogramImages
+
+Simple echogram displays of arbitrary matrices in Julia.
+
+Works in IJulia or any Julia development environment.
+
+A bit like EchogramPlots.jl but generates Images instead of graphs.
+
+A bit like imagesc in MATLAB.
+
+## Simple example
+
+```
+using EchogramImages
+echogram(rand(100,100))
+```
+
+## EK60 example
+
+A useful companion to the SimradEK60.jl package.
+
+```
+using EchogramImages
+using SimradRaw
+using SimradEK60
+using Images
+ps =collect(pings(EK60_SAMPLE))
+ps38 = [p for p in ps if p.frequency == 38000]
+Sv38 = Sv(ps38)
+img = echogram(Sv38)
+imresize(img,(480,640))
+```
+
+You can also use other color schemes
+
+```
+using ColorSchemes
+img = echogram(Sv38,color=ColorSchemes.plasma)
+imresize(img,(480,640))
+```
+
+## Acknowledgements
+
+My thanks to Tim Holy for [sample code](https://discourse.julialang.org/t/how-to-convert-a-matrix-to-an-rgb-image-using-images-jl/7265/8).
--- src/EchogramImages.jl
+++ src/EchogramImages.jl
@@ -1,5 +1,48 @@
 module EchogramImages
 
-# package code goes here
+using Images
+using MappedArrays
+using IndirectArrays
+
+export EK500_COLOURS, echogram
+
+const EK500_COLOURS = [RGB(1, 1, 1),
+                             RGB(159/255, 159/255, 159/255),
+                             RGB(95/255, 95/255, 95/255),
+                             RGB(0, 0, 1),
+                             RGB(0, 0, 127/255),
+                             RGB(0, 191/255, 0),
+                             RGB(0, 127/255, 0),
+                             RGB(1, 1, 0),
+                             RGB(1, 127/255, 0),
+                             RGB(1, 0, 191/255),
+                             RGB(1, 0, 0),
+                             RGB(166/255, 83/255, 60/255),
+                             RGB(120/255, 60/255, 40/255)]
+
+"""
+
+
+"""
+function echogram(A;
+                  vmin = nothing,
+                  vmax = nothing,
+                  color=EK500_COLOURS,
+                  size=(640,480))
+
+
+    if vmax != nothing && vmin != nothing
+        A = clamp.(A,vmin,vmax)
+    end
+
+    x = minimum(A)
+    y = maximum(A)
+    A = (A .- x) ./ (y - x)
+    n = length(color)
+    f = s->clamp(round(Int, (n-1)*s)+1, 1, n)  # safely convert 0-1 to 1:n
+    Ai = mappedarray(f, A)       # like f.(A) but does not allocate significant memory
+    IndirectArray(Ai, color)      # colormap array
+
+end
 
 end # module
