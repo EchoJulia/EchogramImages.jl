a4bf64fe188f402c468238d2c2171d5185dcd41b
Rob Blackwell <rob@robblackwell.com>
2019-02-21 18:37:18 +0000

    Colors externalised

--- src/EchogramImages.jl
+++ src/EchogramImages.jl
@@ -3,115 +3,46 @@ module EchogramImages
 using Images
 using MappedArrays
 using IndirectArrays
+using EchogramColorSchemes
+using ColorSchemes
 
-export EK80_COLOURS, EK500_COLOURS, imagesc, mat2gray, equalizedbins, quantize
-
-const EK500_COLOURS = [RGB(1, 1, 1),
-                             RGB(159/255, 159/255, 159/255),
-                             RGB(95/255, 95/255, 95/255),
-                             RGB(0, 0, 1),
-                             RGB(0, 0, 127/255),
-                             RGB(0, 191/255, 0),
-                             RGB(0, 127/255, 0),
-                             RGB(1, 1, 0),
-                             RGB(1, 127/255, 0),
-                             RGB(1, 0, 191/255),
-                             RGB(1, 0, 0),
-                             RGB(166/255, 83/255, 60/255),
-                             RGB(120/255, 60/255, 40/255)]
-
-const EK80_COLOURS = reverse([RGB(155/255, 7/255, 11/255),
-                      RGB(154/255, 15/255, 22/255),
-                      RGB(153/255, 23/255, 33/255),
-                      RGB(151/255, 31/255, 45/255),
-                      RGB(150/255, 39/255, 56/255),
-                      RGB(153/255, 44/255, 58/255),
-                      RGB(159/255, 47/255, 56/255),
-                      RGB(165/255, 51/255, 54/255),
-                      RGB(170/255, 54/255, 51/255),
-                      RGB(176/255, 57/255, 49/255),
-                      RGB(184/255, 57/255, 48/255),
-                      RGB(198/255, 51/255, 49/255),
-                      RGB(212/255, 44/255, 50/255),
-                      RGB(226/255, 37/255, 51/255),
-                      RGB(240/255, 30/255, 52/255),
-                      RGB(255/255, 24/255, 54/255),
-                      RGB(254/255, 36/255, 75/255),
-                      RGB(253/255, 48/255, 96/255),
-                      RGB(253/255, 61/255, 118/255),
-                      RGB(252/255, 73/255, 139/255),
-                      RGB(252/255, 85/255, 160/255),
-                      RGB(252/255, 93/255, 153/255),
-                      RGB(252/255, 99/255, 130/255),
-                      RGB(252/255, 105/255, 108/255),
-                      RGB(252/255, 110/255, 85/255),
-                      RGB(252/255, 116/255, 63/255),
-                      RGB(252/255, 128/255, 47/255),
-                      RGB(252/255, 153/255, 46/255),
-                      RGB(253/255, 179/255, 45/255),
-                      RGB(253/255, 204/255, 44/255),
-                      RGB(254/255, 229/255, 43/255),
-                      RGB(255/255, 255/255, 42/255),
-                      RGB(208/255, 231/255, 52/255),
-                      RGB(161/255, 208/255, 62/255),
-                      RGB(114/255, 185/255, 72/255),
-                      RGB(68/255, 162/255, 82/255),
-                      RGB(21/255, 139/255, 92/255),
-                      RGB(10/255, 141/255, 99/255),
-                      RGB(17/255, 156/255, 105/255),
-                      RGB(24/255, 171/255, 111/255),
-                      RGB(30/255, 185/255, 116/255),
-                      RGB(37/255, 200/255, 122/255),
-                      RGB(41/255, 197/255, 129/255),
-                      RGB(40/255, 160/255, 138/255),
-                      RGB(39/255, 123/255, 147/255),
-                      RGB(38/255, 86/255, 156/255),
-                      RGB(37/255, 49/255, 165/255),
-                      RGB(36/255, 12/255, 174/255),
-                      RGB(29/255, 30/255, 189/255),
-                      RGB(22/255, 48/255, 204/255),
-                      RGB(15/255, 66/255, 219/255),
-                      RGB(9/255, 84/255, 234/255),
-                      RGB(9/255, 102/255, 249/255),
-                      RGB(9/255, 103/255, 232/255),
-                      RGB(24/255, 96/255, 197/255),
-                      RGB(39/255, 90/255, 163/255),
-                      RGB(53/255, 83/255, 129/255),
-                      RGB(68/255, 76/255, 94/255),
-                      RGB(82/255, 76/255, 78/255),
-                      RGB(97/255, 88/255, 96/255),
-                      RGB(112/255, 100/255, 114/255),
-                      RGB(126/255, 113/255, 132/255),
-                      RGB(141/255, 125/255, 150/255),
-                      RGB(156/255, 138/255, 168/255),
-                      RGB(255/255, 255/255, 255/255)])
-
+export imagesc, mat2gray, equalizedbins, quantize
 
 
 """
     imagesc(A;
              vmin = nothing,
              vmax = nothing,
-             color=EK500_COLOURS,
+             cmap= nothing,
              size=(480,640))
 
 A is any numeric array
 
-color can be a list of Color or come from ColorSchemes.jl or
-PerceptualColourMaps.
+cmap can be a list of Colors or come from EchogramColorSchemes.jl,
+ColorSchemes.jl or PerceptualColourMaps.jl. The default `nothing`
+currently means use EK80 colours.
 
 Set size = nothing for full resolution.
 
 vmin and vmax are minimum and maximum values.
 
+
 """
 function imagesc(A;
                   vmin = nothing,
                   vmax = nothing,
-                  color=EK80_COLOURS,
+                  cmap= nothing,
                   size=(480,640))
 
 
+    if cmap == nothing
+        cmap = addwhite(EK80)
+    end
+
+    if isa(cmap,ColorScheme)
+        cmap = cmap.colors
+    end
+    
     if size != nothing
         A = imresize(A, size)
     end
@@ -127,13 +58,13 @@ function imagesc(A;
     vmin = convert(AbstractFloat, vmin)
     vmax = convert(AbstractFloat, vmax)
 
-    n = length(color)
+    n = length(cmap)
 
     g = x -> (clamp(isnan(x) ? vmin : x, vmin, vmax) - vmin) / (vmax - vmin)
     f = s->clamp(round(Int, (n-1)*g(s))+1, 1, n)
 
-    Ai = mappedarray(f, A)       # like f.(A) but does not allocate significant memory
-    IndirectArray(Ai, color)     # colormap array
+    Ai = mappedarray(f, A) # like f.(A) but does not allocate significant memory
+    IndirectArray(Ai, cmap) # colormap array
 
 end
 
